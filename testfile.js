9319087dbadaa94c78b60364112b4062d02f76e7
Gerald <geralduoj@gmail.com>
2021-02-17 18:22:10 -0500

    testing foreach with break statements
    used every instead of foreach as break returns an illegal break statement

--- testfile.js
+++ testfile.js
@@ -2,6 +2,8 @@ var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
 
 var readres = [];
+var exists = false;
+var emails = [];
 
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
@@ -9,11 +11,37 @@ MongoClient.connect(url, function(err, db) {
   //Find all documents in the customers collection:
   dbo.collection("students").find({}).toArray(function(err, result) {
     if (err) throw err;
-    console.log(result);
+    
     readres = result;
-    readres.forEach(function(student){
-      console.log(student.email);
-    })
+    emails = listStudents();
+    //console.log(emails);
+    checkStudentExists(emails,"geralduoj@gmail.com");
     db.close();
   });
 });
+
+function listStudents(){
+  var emailArr = [];
+
+  readres.forEach(function(student){
+    emailArr.push(student.email);
+  })
+  return emailArr;
+}
+
+function checkStudentExists(arr,word){
+  var studentExists = false;
+
+  arr.every(function(email){
+
+    switch (email.includes(word)) {
+      case true:
+        console.log("True");
+        break;
+      case false:
+        console.log("False");
+        break;
+    }
+    
+  })
+}
